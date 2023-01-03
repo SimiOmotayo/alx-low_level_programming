a2479ae485d45574c61945641b02478f7c8d79d7
SimiOmotayo <omotkleanbiz@gmail.com>
2023-01-03 07:04:32 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+   * _strpbrk - finds string
+   * @s: string to find stuff
+   * @accept: accepting arrays
+   * Return: returns new value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+
+}
