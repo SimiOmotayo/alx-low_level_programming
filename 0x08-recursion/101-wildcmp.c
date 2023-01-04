c89b52a536af59566f32162cb39bde00320cc7b1
SimiOmotayo <omotkleanbiz@gmail.com>
2023-01-04 14:24:31 -0800

    wild comparison

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,6 @@
    * @j: special index. (joker)
    * Return: 1 if s is palindrome, 0 otherwise.
  */
-
 int str_checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
@@ -17,12 +16,10 @@ int str_checker(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
-	
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));	
 	return (0);
 
 }
-
 /**
    * wildcmp - check if strings could be considered identical
    * @s1: base address for string.
